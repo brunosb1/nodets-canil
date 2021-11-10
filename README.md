69fc640ef77b4efacf5d8e42446485dd51a9902d
Bruno Vrenna <brunovrenna@gmail.com>
2021-11-10 12:13:20 -0300

    base criada

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # nodets-canil
 TypeScript, Node and mustache 
+
+### pré requisistos globais:
+`npm i -g nodemon typescript ts-node`
+
+
+### instalação
+`npm install`
+
+
+### rodar o projeto
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+dotenv.config();
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname,'../public')));
+
+// Rotas
+
+//utilizando a rota home do mainRoutes
+server.use(mainRoutes);
+
+//página nao encontrada
+server.use((req, res)=>{
+    res.send('pagina nao encontrada');
+})
+
+
+
+server.listen(process.env.PORT);
